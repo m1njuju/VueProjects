0dfc6fa4457a569ac532bf9015fcb23cc71555cd
m1njuju <mj6596@naver.com>
2022-04-01 17:36:40 +0900

    appbar 수정중

--- components/toolbar.js
+++ components/toolbar.js
@@ -1,17 +1,116 @@
 Vue.component('toolbar-component',{
+    data: function(){
+        return {
+            ndrawer: false,
+            items: [
+                { title: '설정' },
+                { title: '로그인' },
+                { title: 'Click Me' },
+                { title: 'Click Me 2' },
+            ],
+            menuList: [
+                {
+                    title : '스타벅스',
+                    icon : 'mdi-coffee-maker',
+                },
+                {
+                    title : '애플',
+                    icon : 'mdi-apple',
+                },
+                {
+                    title : '페이스북',
+                    icon : 'mdi-facebook',
+                }
+            ],
+            focus: '',
+            type: 'month',
+            closeOnContentClick: false,
+        }
+    },
+    computed: {
+        title: function(){ return this.$refs.calendar.title}
+    },
+    mounted () {
+        this.$refs.calendar.checkChange()
+    },
+    methods: {
+        setToday () {
+            this.focus = ''
+        },
+        prev () {
+            this.$refs.calendar.prev()
+        },
+        next () {
+            this.$refs.calendar.next()
+        },
+    },
     template:`
+    <div>
     <!-- 상단 툴바 -->
-            <v-app-bar app >
-                <v-app-bar-nav-icon></v-app-bar-nav-icon>
-                <v-toolbar-title>오늘의 일기</v-toolbar-title>
+            <v-app-bar app color="info lighten-2" dark>
+                <v-app-bar-nav-icon @click="ndrawer = true"></v-app-bar-nav-icon>
+                <v-toolbar-title >오늘의 일기</v-toolbar-title>
 
                 
                 <v-spacer></v-spacer>
 
                 <!-- 날짜 선택으로 검색할 수 있게 -->
-                <v-btn icon>
+
+                <v-menu bottom left :close-on-content-click="closeOnContentClick"
+                class="position:relative;overflow-x:hidden;">
+                <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    dark
+                    icon
+                    v-bind="attrs"
+                    v-on="on"
+                >
                     <v-icon>mdi-calendar-month-outline</v-icon>
                 </v-btn>
+                </template>
+
+                <!-- 캘린더 부분 -->
+                <div>
+                    <v-row>
+                        <v-col>
+                            <v-sheet>
+                                <v-toolbar flat>
+                                    <v-btn outlined class="mr-4"
+                                        color="info lighten-2" @click="setToday">Today</v-btn>
+
+                                    <v-btn fab text small
+                                        color="info lighten-2" @click="prev">
+                                        <v-icon> mdi-chevron-left </v-icon>
+                                    </v-btn>
+
+                                    <v-btn fab text small
+                                        color="info lighten-2" @click="next">
+                                        <v-icon> mdi-chevron-right </v-icon>
+                                    </v-btn>
+                                        
+                                    <v-toolbar-title v-if="$refs.calendar">
+                                        {{ $refs.calendar.title }}
+                                    </v-toolbar-title>
+                                </v-toolbar>
+                            </v-sheet>
+                            
+                            <v-sheet width="300">
+                                <v-calendar
+                                ref="calendar"
+                                v-model="focus"
+                                color="info lighten-2"
+                                :type="type"
+                                ></v-calendar>
+                            </v-sheet>
+                        </v-col>
+                    </v-row>
+                </div>
+                <!-- 캘린더 끝 -->
+            </v-menu>
+
+                
+
+                
 
                 <!--사진만 보일 시 나타날 아이콘-->
                 <v-btn icon>
@@ -27,6 +126,66 @@ Vue.component('toolbar-component',{
                 <v-btn icon>
                     <v-icon>mdi-plus-circle-outline</v-icon>
                 </v-btn>
+
+                <v-menu
+                bottom
+                left
+                >
+                <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    dark
+                    icon
+                    v-bind="attrs"
+                    v-on="on"
+                >
+                    <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+                </template>
+
+                <v-list>
+                <v-list-item
+                    v-for="(item, i) in items"
+                    :key="i"
+                >
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+                </v-list>
+            </v-menu>
             </v-app-bar>
+
+            
+
+            <!--내비게이션 서랍 시작 -->
+            <v-navigation-drawer absolute temporary v-model="ndrawer">
+                <v-toolbar flat>
+                    <v-list>
+                        <v-list-item>
+                            <v-list-item-avatar>
+                                <v-icon class="grey white--text">mdi-account</v-icon>
+                            </v-list-item-avatar>
+
+                            <v-list-item-content>
+                                <v-list-item-title> 닉네임 </v-list-item-title>
+                                <v-list-item-subtitle> 아이디 </v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                </v-toolbar>
+
+                <v-divider></v-divider>
+                <!-- 내비게이션 서랍 메뉴 리스트 -->
+                <v-list>
+                    <v-list-item @click="" v-for="menu in menuList">
+                        <v-list-item-action>
+                            <v-icon>{{menu.icon}}</v-icon>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                            <v-list-item-title>{{menu.title}}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
+            </v-navigation-drawer>
+    </div>
     `
 })
--- components/write.js
+++ components/write.js
@@ -1,5 +1,102 @@
 Vue.component('write-component',{
     template:`
-    
+    <div >
+    <!--일기 작성란 -->
+    <v-container>
+        <v-card max-width="1000px">
+
+            <v-row>
+                <v-col cols="4">
+                    <v-img 
+                    :src="'https://picsum.photos/500/300?image'"
+                    :lazy-src="'https://picsum.photos/10/6?image='+n * 15"
+                    aspect-ratio="1"
+                    class="grey lighten-2 .float-left ml-5 mt-5"
+                    max-width="200px"
+                    @click=""
+                    >
+
+                    <!--이미지 로딩 진행 써클 -->
+                    <template v-slot:placeholder>
+                        <v-row
+                            class="fill-height ma-0"
+                            align="center"
+                            justify="center"
+                        >
+                            <v-progress-circular
+                            indeterminate
+                            color="grey lighten-5"
+                            ></v-progress-circular>
+                        </v-row>
+                    </template>
+
+                    </v-img>
+                </v-col>
+
+                <v-col cols="8">
+                    <!--날짜 입력 -->
+                    <v-card-text><input type="date"></v-card-text>
+
+                    <!-- 기분 선택칸 -->
+                    <v-card-text>
+                        오늘의 기분
+                        <v-row justify="space-around">
+                            <v-col
+                            cols="12"
+                            sm="10"
+                            md="8"
+                            >
+                                <v-chip-group
+                                mandatory
+                                active-class="pink--text"
+                                >
+                                    <v-chip
+                                    v-for="tag in tags"
+                                    :key="tag"
+                                    >
+                                    <v-icon>
+                                        {{ tag }}
+                                    </v-icon>
+                                    </v-chip>
+                                </v-chip-group>
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
+
+                    <v-card-text>
+                        <div>오늘의 할 일   <input type="text" style="position: relative; left:60px;">
+                            <v-card-action>
+                                <v-btn icon style="position: relative; left:60px;">
+                                    <v-icon dark>mdi-pencil</v-icon>
+                                </v-btn>
+                            </v-card-action>
+                        </div><br>
+                        <div>오늘 먹은 것  <input type="text" style="position: relative; left:60px;"> 
+                            <v-card-action>
+                                <v-btn icon style="position: relative; left:60px;">
+                                    <v-icon dark>mdi-pencil</v-icon>
+                                </v-btn>
+                            </v-card-action>
+                        </div>
+                    </v-card-text>
+                </v-col>
+            </v-row>
+
+            <v-divider class="mx-4"></v-divider>
+
+            <!-- 긴 문장 작성 칸 -->
+            <v-card-text>
+                <div>
+                    <textarea style="resize:none;" cols="90" rows="20" placeholder="오늘의 일기를 작성해 주세요"></textarea>
+                </div>
+            </v-card-text>
+
+            <v-card-actions >
+                <v-spacer></v-spacer>
+                <v-btn justify-center>완료</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-container>
+    </div>
     `
 })
